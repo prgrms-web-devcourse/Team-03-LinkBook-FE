93145f85175335eda9e8b6c63fc6122178aeff00
Steadystudy <sgmin206@naver.com>
2022-07-27 19:46:08 +0900

    feat: Avatar 컴포넌트 구현

---
+++ web/components/Avatar/Avatar.style.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+
+interface Props {
+  size: number;
+}
+
+export const AvatarWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const AvatarImg = styled(Image)`
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.gray[3]};
+`;
+
+export const AvatarName = styled.span`
+  margin-left: 0.5rem;
+  font-size: ${({ theme }) => theme.fontSize.c[0]};
+`;
