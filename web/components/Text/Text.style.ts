68bdd4c2b491ab51b2edaf70e10837ffe894104e
YongSangYou <dydtkd113@gmail.com>
2022-07-27 18:12:52 +0900

    feat: Text 컴포넌트 구현

---
+++ web/components/Text/Text.style.ts
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+interface Props {
+  children: React.ReactNode;
+  size?: string | number;
+  color?: string;
+  weight?: string | number;
+  version?: string;
+  fontFamily?: string;
+}
+
+const Header = ({ theme }: ThemeProps) => css`
+  font-size: ${theme.fontSize.t[0]};
+  font-weight: bold;
+`;
+
+export const Text = styled.span`
+  font-size: ${({ size }: Props) => size};
+  weight: ${({ weight }: Props) => weight};
+  color: ${({ color }: Props) => color};
+  font-family: ${({ fontFamily }: Props) => fontFamily};
+
+  ${({ version }: Props) => {
+    switch (version) {
+      case 'header':
+        return Header;
+      default:
+        return;
+    }
+  }};
+`;
