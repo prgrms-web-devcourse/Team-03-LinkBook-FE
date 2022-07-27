8bd9cd7c94d173a12e32080e05ad9b182deccfc9
Steadystudy <sgmin206@naver.com>
2022-07-27 19:45:19 +0900

    feat: Icon 컴포넌트 구현

---
+++ web/components/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+interface Props {
+  name: string;
+  size: string;
+}
+
+const defaultProps = {
+  size: '40',
+  name: '',
+};
+
+const Icon = ({ size, name }: Props) => {
+  return (
+    <Image src={`/icons/${name}.svg`} width={size} height={size} alt="icon" />
+  );
+};
+
+Icon.defaultProps = defaultProps;
+
+export default Icon;
