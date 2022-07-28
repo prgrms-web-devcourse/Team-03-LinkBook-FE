96df4e8f3452eb0783ce839f3e4b5c65b6fcdead
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-29 00:42:31 +0900

    Design: Footer SNSButton Logo 추가

--- web/components/Footer/SNSButton/SNSButton.style.ts
+++ web/components/Footer/SNSButton/SNSButton.style.ts
@@ -3,12 +3,19 @@ import styled from '@emotion/styled';
 export const Container = styled.button`
   display: flex;
   gap: 10px;
+  align-items: center;
   background-color: transparent;
   border: none;
+  border-radius: 10px;
+  padding: 8px;
   cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.gray[5]};
+  }
 `;
 
 export const Text = styled.p`
-  font-weight: 500;
+  font-weight: 600;
   font-size: ${({ theme }) => theme.fontSize.h[2]};
 `;
