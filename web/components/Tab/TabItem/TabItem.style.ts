a8b5b5cfce457613bb5103a93343baac401531c4
960817 <92906766+960817@users.noreply.github.com>
2022-07-28 17:09:45 +0900

    Design : TabItem 컴포넌트 font-weight 추가

--- web/components/Tab/TabItem/TabItem.style.ts
+++ web/components/Tab/TabItem/TabItem.style.ts
@@ -8,6 +8,7 @@ export const TabItemWrapper = styled.div<{ selected: boolean }>`
   font-size: ${({ theme }) => theme.fontSize.b[0]};
   color: ${({ theme, selected }) =>
     selected ? theme.colors.white : theme.colors.gray[3]};
+  font-weight: 500;
 
   background-color: ${({ theme, selected }) =>
     selected ? theme.colors.gray[3] : theme.colors.white};
