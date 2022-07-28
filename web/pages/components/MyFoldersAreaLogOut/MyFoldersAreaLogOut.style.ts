856eb5364c96bae9097adb29122da18dcc124428
Steadystudy <sgmin206@naver.com>
2022-07-28 16:37:43 +0900

    feat: 비로그인시 MyFolderArea 컴포넌트 구현

---
+++ web/pages/components/MyFoldersAreaLogOut/MyFoldersAreaLogOut.style.ts
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import { Button, Text } from '../../../components';
+
+export const AreaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 1200px;
+  height: 932px;
+`;
+
+export const BackgroundImage = styled(Image)`
+  position: relative;
+`;
+
+export const ContentContainer = styled.div`
+  position: absolute;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+`;
+
+export const Header = styled(Text)`
+  font-size: ${({ theme }) => theme.fontSize.l[1]};
+  font-weight: 700;
+  text-align: center;
+  line-height: 52px;
+`;
+
+export const MainColor = styled(Text)`
+  font-size: ${({ theme }) => theme.fontSize.l[1]};
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.main[0]};
+`;
+
+export const Description = styled(Text)`
+  text-align: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  gap: 8px;
+`;
+
+export const SignUpButton = styled(Button)`
+  background-color: ${({ theme }) => theme.colors.mainLight[0]};
+`;
