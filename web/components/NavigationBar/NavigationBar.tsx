382f9f0f0e8eb709253b00c92a670152fd9095e5
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-28 22:55:43 +0900

    Feat: isLoggedIn state
    - useRecoilState 사용한 상태 관리로 변경 예정

--- web/components/NavigationBar/NavigationBar.tsx
+++ web/components/NavigationBar/NavigationBar.tsx
@@ -1,10 +1,26 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import Avatar from '../Avatar';
 import Button from '../Button';
 import Icon from '../Icon';
 import Text from '../Text';
 import * as S from './NavigationBar.style';
 
-const NavigationBar = () => {
+interface Props {
+  isLogin: boolean;
+}
+
+const defaultProps = {
+  isLogin: false,
+};
+
+const NavigationBar = ({ isLogin }: Props) => {
+  const [isLoggedIn, setisLoggedIn] = useState(isLogin);
+
+  useEffect(() => {
+    setisLoggedIn(isLogin);
+  }, [isLogin]);
+
   return (
     <S.Container>
       <S.ItemContainer>
@@ -22,16 +38,35 @@ const NavigationBar = () => {
             <S.NavItem>이용방법</S.NavItem>
           </Link>
         </S.Nav>
-        <S.Line>|</S.Line>
-        <S.User>
-          <S.UserSignUp>회원가입</S.UserSignUp>
-          <Button type="button" version="navBar">
-            로그인
-          </Button>
-        </S.User>
+        {isLoggedIn ? (
+          <>
+            <Avatar size={35} />
+            <S.Line>|</S.Line>
+            <S.UserContainer>
+              <Link href="/mypage" passHref>
+                <S.NavItem>마이페이지</S.NavItem>
+              </Link>
+              <Button type="button" version="navBar">
+                새 폴더 작성
+              </Button>
+            </S.UserContainer>
+          </>
+        ) : (
+          <>
+            <S.Line>|</S.Line>
+            <S.UserContainer>
+              <S.UserButton>회원가입</S.UserButton>
+              <Button type="button" version="navBar">
+                로그인
+              </Button>
+            </S.UserContainer>
+          </>
+        )}
       </S.ItemContainer>
     </S.Container>
   );
 };
+
+NavigationBar.defaultProps = defaultProps;
 
 export default NavigationBar;
