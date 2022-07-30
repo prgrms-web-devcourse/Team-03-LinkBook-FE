6be6e6bbbd47ea73aa408a175fb03e70f28bebda
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-31 01:50:26 +0900

    Fix: DummyData에 맞춰 Profile Component 수정
    - DummyData UserInfo -> introduce 추가

--- web/components/Profile/Profile.tsx
+++ web/components/Profile/Profile.tsx
@@ -1,16 +1,29 @@
+import { UserInfo } from '../../shared/DummyDataType';
 import { Avatar } from '../index';
 import * as S from './Profile.style';
 
-interface UserProps {
-  name: string;
-  introduce?: string;
-  date?: string;
-}
+// comments: [
+// 	{
+// 		id: 1,
+// 		parentId: null,
+// 		content: '도움이 많이 되었습니다.',
+// 		user: {
+// 			id: 2,
+// 			name: '이지연',
+// 			image:
+// 				'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+// 			introduce: '한 줄 소개'
+// 		},
+// 		createdAt: '',
+// 		updatedAt: ' ',
+// 	},
+// ],
 
 interface Props {
   iconSize: number | string;
   version: 'comment' | 'profile';
-  user: UserProps;
+  user: UserInfo;
+  createdAt?: string;
 }
 
 const defaultProps = {
@@ -18,21 +31,18 @@ const defaultProps = {
   version: 'profile',
 };
 
-const Profile = ({ iconSize, version, user }: Props) => {
-  const { name, introduce, date } = user;
+const Profile = ({ iconSize, version, user, createdAt }: Props) => {
+  const { name, introduce } = user;
 
   return (
     <S.Container>
       <S.IconContainer size={iconSize}>
-        <Avatar
-          // src="https://avatars.githubusercontent.com/u/72294509?v=4"
-          size={iconSize}
-        />
+        <Avatar size={iconSize} />
       </S.IconContainer>
       <S.TextContainer>
         <S.Name version={version}>{name}</S.Name>
         <S.Description version={version}>
-          {introduce ? introduce : date}
+          {createdAt ? createdAt : introduce}
         </S.Description>
       </S.TextContainer>
     </S.Container>
