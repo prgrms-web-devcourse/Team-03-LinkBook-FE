db3619310899fa26cc1ab18ba3de41d2fe135300
960817 <92906766+960817@users.noreply.github.com>
2022-07-30 15:03:07 +0900

    Feat: Skeleton 컴포넌트 구현, Category컴포넌트에 Card, Skeleton 적용

--- web/components/Skeleton/Skeleton.tsx
+++ web/components/Skeleton/Skeleton.tsx
@@ -1,54 +1,21 @@
-import styled from '@emotion/styled';
-
-interface Container {
-  width: string | number;
-  height: string | number;
-}
-
-const SkeletonContainer = styled.div<Container>`
-  display: inline-block;
-  border-radius: 4px;
-
-  background-image: linear-gradient(
-    90deg,
-    #dfe3e8 0px,
-    #efefef 40px,
-    #dfe3e8 80px
-  );
-  background-size: 200% 100%;
-  background-position: 0 center;
-  animation: skeleton--zoom-in 0.2s ease-out,
-    skeleton--loading 2s infinite linear;
-  @keyframes skeleton--zoom-in {
-    0% {
-      opacity: 0;
-      transform: scale(0.95);
-    }
-    100% {
-      opacity: 1;
-      transform: scale(1);
-    }
-  }
-  @keyframes skeleton--loading {
-    0% {
-      background-position-x: 100%;
-    }
-    50% {
-      background-position-x: -100%;
-    }
-    100% {
-      background-position-x: -100%;
-    }
-  }
-`;
+import * as S from './Skeleton.style';
 
 interface Props {
   width: string | number;
   height: string | number;
+  repeat: number;
 }
 
-const Skeleton = ({ width, height }: Props) => {
-  return <SkeletonContainer width={width} height={height} />;
+const Skeleton = ({ width, height, repeat }: Props) => {
+  const repeatArr = Array.from({ length: repeat }, (_, index) => index);
+
+  return (
+    <>
+      {repeatArr.map((num) => (
+        <S.SkeletonContainer key={num} width={width} height={height} />
+      ))}
+    </>
+  );
 };
 
 export default Skeleton;
--- web/next.config.js
+++ web/next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
