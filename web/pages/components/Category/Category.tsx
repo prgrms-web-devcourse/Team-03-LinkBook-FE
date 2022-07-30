cf6b42d4c7762fa324f704550d755245aa0dd50a
960817 <92906766+960817@users.noreply.github.com>
2022-07-30 15:16:10 +0900

    Feat: CateogoryCardDummy Data 추가

--- web/pages/components/Category/Category.tsx
+++ web/pages/components/Category/Category.tsx
@@ -1,5 +1,6 @@
 import * as S from './Category.style';
 import { Card, Skeleton, Tab } from '../../../components';
+import { CardDummyData } from '../../../shared/categoryCardDummy';
 
 interface Item {
   id: string;
@@ -13,97 +14,7 @@ interface Props {
   isLoading?: boolean;
 }
 
-const Category = ({
-  onClick,
-  selectedItem,
-  tabItem,
-  isLoading = true,
-}: Props) => {
-  const data = [
-    {
-      id: 1,
-      image:
-        'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-      title: '프론트엔드 개발자를 위한 학습 로드맵',
-      tags: [
-        { id: '1', text: '프론트엔드 개발자' },
-        { id: '2', text: '개발자' },
-        { id: '3', text: '프론트엔드' },
-      ],
-      user: {
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        name: 'Miral',
-      },
-      createdAt: '2022-07-28',
-      likes: 154,
-      bookmarks: [
-        {
-          id: 1,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Naver Blog - 프론트엔드 학습 어쩌구 저쩌구',
-          url: 'https://www.naver.com/',
-        },
-        {
-          id: 2,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 3,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 4,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 5,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 6,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 7,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 8,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-        {
-          id: 9,
-          image:
-            'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-          title: 'Baekjoon Online Judge',
-          url: 'https://www.acmicpc.net/',
-        },
-      ],
-    },
-  ];
+const Category = ({ onClick, selectedItem, tabItem, isLoading }: Props) => {
   return (
     <S.CategoryWrapper>
       <S.DescriptionText>
@@ -114,9 +25,9 @@ const Category = ({
       </S.TabWrapper>
       <S.CategoryCardWrapper>
         {isLoading ? (
-          data.map((item) => <Card data={item} />)
+          <Skeleton width={340} height={400} repeat={CardDummyData.length} />
         ) : (
-          <Skeleton width={340} height={400} repeat={data.length} />
+          CardDummyData.map((item) => <Card data={item} />)
         )}
       </S.CategoryCardWrapper>
       <S.BtnWrapper>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,105 +1,11 @@
 import type { NextPage } from 'next';
 import * as S from './index.style';
-import { Category, MyFoldersAreaLogOut } from './components';
-import { Card } from '../components';
-import Skeleton from '../components/Skeleton/Skeleton';
+import { MyFoldersAreaLogOut } from './components';
 
-// const MainPage: NextPage = () => {
-//   return (
-//     <S.Div>
-//       <MyFoldersAreaLogOut />
-//     </S.Div>
-//   );
-// };
 const MainPage: NextPage = () => {
-  const data = {
-    id: 1,
-    image:
-      'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-    title: '프론트엔드 개발자를 위한 학습 로드맵',
-    tags: [
-      { id: '1', text: '프론트엔드 개발자' },
-      { id: '2', text: '개발자' },
-      { id: '3', text: '프론트엔드' },
-    ],
-    user: {
-      image:
-        'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-      name: 'Miral',
-    },
-    createdAt: '2022-07-28',
-    likes: 154,
-    bookmarks: [
-      {
-        id: 1,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Naver Blog - 프론트엔드 학습 어쩌구 저쩌구',
-        url: 'https://www.naver.com/',
-      },
-      {
-        id: 2,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 3,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 4,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 5,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 6,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 7,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 8,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-      {
-        id: 9,
-        image:
-          'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-        title: 'Baekjoon Online Judge',
-        url: 'https://www.acmicpc.net/',
-      },
-    ],
-  };
   return (
     <S.Div>
-      {/* <Skeleton width={340} height={400} /> */}
-      {/* <Card data={data} /> */}
-      <Category />
+      <MyFoldersAreaLogOut />
     </S.Div>
   );
 };
