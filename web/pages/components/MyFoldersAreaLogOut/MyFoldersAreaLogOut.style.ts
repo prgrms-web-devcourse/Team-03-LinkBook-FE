42555a6dc43c37ffab7c7487631d261d12e71991
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-31 20:46:51 +0900

    DesignL MyFoldersAreaLogOut Component style 수정

--- web/pages/components/MyFoldersAreaLogOut/MyFoldersAreaLogOut.style.ts
+++ web/pages/components/MyFoldersAreaLogOut/MyFoldersAreaLogOut.style.ts
@@ -44,6 +44,7 @@ export const Description = styled(Text)`
   font-size: ${({ theme }) => theme.fontSize.h[2]};
   font-weight: 500;
   text-align: center;
+  line-height: 1.4;
 `;
 
 export const ButtonWrapper = styled.div`
