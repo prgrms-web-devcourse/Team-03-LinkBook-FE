07d16027a7aa37289d25bb9325e3bb289a1c83d3
Jungjjeong <sky990106@dgu.ac.kr>
2022-08-02 01:13:23 +0900

    Chore: 필요하지 않은 주석 삭제

--- web/components/Profile/Profile.tsx
+++ web/components/Profile/Profile.tsx
@@ -3,23 +3,6 @@ import { UserInfo } from '../../shared/DummyDataType';
 import { Avatar } from '../index';
 import * as S from './Profile.style';
 
-// comments: [
-// 	{
-// 		id: 1,
-// 		parentId: null,
-// 		content: '도움이 많이 되었습니다.',
-// 		user: {
-// 			id: 2,
-// 			name: '이지연',
-// 			image:
-// 				'https://images.unsplash.com/photo-1515041219749-89347f83291a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-// 			introduce: '한 줄 소개'
-// 		},
-// 		createdAt: '',
-// 		updatedAt: ' ',
-// 	},
-// ],
-
 interface Props {
   iconSize: number | string;
   version: 'comment' | 'profile';
