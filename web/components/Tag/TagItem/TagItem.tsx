7909fa759081baaba2bbf91e8f9231b36d30a96b
YongSangYou <dydtkd113@gmail.com>
2022-08-02 18:02:59 +0900

    pick feat: TagItem 컴포넌트 handleRemoveTag 추가, Icon 렌더링

--- web/components/Tag/TagItem/TagItem.tsx
+++ web/components/Tag/TagItem/TagItem.tsx
@@ -1,16 +1,28 @@
 import { MouseEventHandler } from 'react';
+import Icon from '../../Icon';
 import * as S from './TagItem.style';
 
 interface Props {
   children: string;
   onClick?: MouseEventHandler;
   shrinking?: boolean;
+  handleRemoveTag?: (value: string) => void;
 }
 
-const TagItem = ({ children, onClick, shrinking = false }: Props) => {
+const TagItem = ({
+  children,
+  onClick,
+  shrinking = false,
+  handleRemoveTag,
+}: Props) => {
   return (
     <S.TagItem shrinking={shrinking} onClick={onClick}>
       {children}
+      {handleRemoveTag && (
+        <S.IconWrapper onClick={() => handleRemoveTag(children)}>
+          <Icon name="x" size={9} />
+        </S.IconWrapper>
+      )}
     </S.TagItem>
   );
 };
