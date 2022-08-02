2b70188ead0e58536adb687df2c2048efcd10a03
YongSangYou <dydtkd113@gmail.com>
2022-08-02 18:02:21 +0900

    pick feat: Tag 컴포넌트 handleRemoveTag prop 추가

--- web/components/Tag/Tag.tsx
+++ web/components/Tag/Tag.tsx
@@ -4,26 +4,32 @@ import * as S from './Tag.style';
 interface Props {
   tagItems: string[];
   shrinking?: boolean;
+  handleRemoveTag?: (value: string) => void;
 }
 
-const Tag = ({ tagItems, shrinking = false }: Props) => {
+const Tag = ({ tagItems, shrinking = false, handleRemoveTag }: Props) => {
   return shrinking && tagItems.length > 3 ? (
     <S.TagWrapper>
       {tagItems.slice(0, 3).map((item, index) => (
-        <TagItem shrinking key={index}>
+        <TagItem shrinking key={index} handleRemoveTag={handleRemoveTag}>
           {item}
         </TagItem>
       ))}
-      <TagItem key={123}>{`+${tagItems.length - 3}`}</TagItem>
+      <TagItem>{`+${tagItems.length - 3}`}</TagItem>
     </S.TagWrapper>
   ) : (
     <S.TagWrapper>
       {tagItems.map((item, index) => (
-        <TagItem shrinking={shrinking} key={index}>
+        <TagItem
+          shrinking={shrinking}
+          key={index}
+          handleRemoveTag={handleRemoveTag}
+        >
           {item}
         </TagItem>
       ))}
     </S.TagWrapper>
   );
 };
+
 export default Tag;
