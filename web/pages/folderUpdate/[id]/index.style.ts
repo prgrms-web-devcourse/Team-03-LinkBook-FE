ddc145cffe511052c0b12ab77faac0e708784648
Steadystudy <sgmin206@naver.com>
2022-08-03 03:55:28 +0900

    feat: folder update page 초안

---
+++ web/pages/folderUpdate/[id]/index.style.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { Input, Button } from '../../../components';
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 80px auto;
+  gap: 24px;
+`;
+
+export const TitleInput = styled(Input)`
+  margin-top: -16px;
+  font-size: ${({ theme }) => theme.fontSize.t[0]};
+  font-weight: 700;
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.gray[3]};
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 80px;
+  gap: 16px;
+`;
+
+export const BookmarkButton = styled(Button)`
+  width: 160px;
+  color: ${({ theme }) => theme.colors.gray[3]};
+  background-color: ${({ theme }) => theme.colors.white[0]};
+  border: 1px solid ${({ theme }) => theme.colors.gray[3]}; ;
+`;
