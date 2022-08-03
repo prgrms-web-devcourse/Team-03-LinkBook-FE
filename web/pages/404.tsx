5e868dabfc27e6caa8e70b88fe1cea99b88eaa4f
Steadystudy <sgmin206@naver.com>
2022-08-03 16:47:27 +0900

    feat: 404 페이지 구현(초안)

---
+++ web/pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import * as S from './index.style';
+
+const NotFoundPage: NextPage = () => {
+  return (
+    <S.NotFoundContainer>
+      404페이지입니다~! 이미지 넣을 예정
+    </S.NotFoundContainer>
+  );
+};
+
+export default NotFoundPage;
