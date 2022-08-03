e4b2dd4c11a949c1c022e86906b26f59610aa1f5
Steadystudy <sgmin206@naver.com>
2022-08-03 16:47:27 +0900

    feat: 404 페이지 구현(초안)

--- web/pages/index.style.ts
+++ web/pages/index.style.ts
@@ -1,5 +1,12 @@
 import styled from '@emotion/styled';
 
+export const NotFoundContainer = styled.div`
+  max-width: 1200px;
+  margin: 200px;
+
+  font-size: ${({ theme }) => theme.fontSize.h[0]};
+`;
+
 export const Div = styled.div`
   width: fit-content;
   margin: 0 auto;
