61cbf9817f58e7f3432854137edb2d5c2aa3c9bc
Steadystudy <sgmin206@naver.com>
2022-08-06 01:04:46 +0900

    feat: folder response type 설정

---
+++ web/types/folder.ts
@@ -0,0 +1,69 @@
+interface User {
+  id: number;
+  name: string;
+  image: string | null;
+}
+
+interface Sort {
+  unsorted: boolean;
+  empty: boolean;
+  sorted: boolean;
+}
+
+interface Folder {
+  id: number;
+  title: string;
+  image: string;
+  isPinned: boolean;
+  isPrivate: boolean;
+  user: User;
+  tags: string[];
+  likes: number;
+  createdAt: string;
+}
+
+interface Page {
+  sort: Sort;
+  offset: number;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+interface Bookmark {
+  id: number;
+  url: string;
+  title: string;
+}
+
+// 전체 폴더리스트 조회
+export interface AllFolderList {
+  content: Folder[];
+  pageable: Page;
+  last: boolean;
+  totalElements: number;
+  totalPages: number;
+  numberOfElements: number;
+  sort: Sort;
+  number: number;
+  first: boolean;
+  size: number;
+  empty: boolean;
+}
+
+// 특정사용자의 폴더리스트 조회
+export interface SpecificUserFolderList {
+  userResponse: User;
+  folders: AllFolderList;
+}
+
+//특정 폴더 조회
+export interface SpecificFolder extends Folder {
+  bookmarks: Bookmark[];
+}
+
+// 폴더 생성 또는 수정 후 받는 response
+export interface FolderCreateOrUpdate {
+  id: number;
+}
