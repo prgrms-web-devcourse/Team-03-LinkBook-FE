76796d269b98068f86d75ee4a88671b8a3bf9b6f
960817 <92906766+960817@users.noreply.github.com>
2022-08-11 23:48:55 +0900

    Style: setCookie 함수 수정

--- extension/src/utils/cookies.ts
+++ extension/src/utils/cookies.ts
@@ -1,29 +1,19 @@
-export const setCookie = (
+export const setCookie = async (
   key: string,
   domain: string,
   url: string,
   value: string
 ) => {
-  return new Promise((resolve, reject) => {
-    chrome.cookies.set(
-      {
-        name: key,
-        domain,
-        url,
-        expirationDate: new Date().getTime() / 1000 + 3600,
-        value: value,
-      },
-      (cookie) => {
-        if (cookie) {
-          console.log(cookie);
-          resolve(cookie.value);
-        } else {
-          console.log("Can't get cookie");
-          reject(0);
-        }
-      }
-    );
+  const res = await chrome.cookies.set({
+    name: key,
+    domain,
+    url,
+    expirationDate: new Date().getTime() / 1000 + 3600,
+    value,
   });
+
+  console.log(res?.value);
+  return res?.value;
 };
 
 export const getCookie = async (key: string, url: string) => {
