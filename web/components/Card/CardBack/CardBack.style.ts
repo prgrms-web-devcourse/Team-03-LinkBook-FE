8fe7b7fd35aeecc3d221878ffced27358d1f396a
YongSangYou <dydtkd113@gmail.com>
2022-08-12 00:10:13 +0900

    design: 배경색 추가

--- web/components/Card/CardBack/CardBack.style.ts
+++ web/components/Card/CardBack/CardBack.style.ts
@@ -14,24 +14,27 @@ export const Card = styled.div<Props>`
   height: 403px;
   border-radius: 10px;
   padding: 16px 16px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  background-color: ${({ theme }) => theme.colors.white[0]};
   transition-duration: 1s;
-  transform: ${({reverseCard}) => reverseCard ? 'rotateY(0)' : 'rotateY(-180deg)'};
+  transform: ${({ reverseCard }) =>
+    reverseCard ? 'rotateY(0)' : 'rotateY(-180deg)'};
   backface-visibility: hidden;
 `;
 
 export const IconWrapper = styled.div`
   display: inline;
   cursor: pointer;
-`
+`;
 
 export const BookmarkList = styled.ul<Props>`
   height: 340px;
   padding-top: 8px;
   /* overflow: auto; */
-  overflow: ${({reverseCard}) => reverseCard ? 'auto' : 'hidden'};
-  overscroll-behavior-y: ${({reverseCard}) => reverseCard ? 'contain' : 'auto'};
+  overflow: ${({ reverseCard }) => (reverseCard ? 'auto' : 'hidden')};
+  overscroll-behavior-y: ${({ reverseCard }) =>
+    reverseCard ? 'contain' : 'auto'};
   /* 스크롤바 전체 */
   &::-webkit-scrollbar {
     width: 8px;
@@ -42,7 +45,7 @@ export const BookmarkList = styled.ul<Props>`
     border-radius: 10px;
     background-color: ${(props) => props.theme.colors.gray[4]};
   }
-`
+`;
 
 export const Bookmark = styled.li`
   display: flex;
@@ -55,10 +58,10 @@ export const Bookmark = styled.li`
   &:hover {
     background-color: ${(props) => props.theme.colors.gray[5]};
   }
-`
+`;
 
 export const TextWrapper = styled.div`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-`
+`;
