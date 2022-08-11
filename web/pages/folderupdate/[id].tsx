e309b01fa16f7262c71fe60276da6f5a844437fb
Steadystudy <sgmin206@naver.com>
2022-08-11 15:31:43 +0900

    feat: folderupdate api 불러오기 및 스타일 일부 수정

--- web/pages/folderupdate/[id].tsx
+++ web/pages/folderupdate/[id].tsx
@@ -1,40 +1,76 @@
 import * as S from '../../styles/pageStyles/folderUpdate.style';
 import {
-  Input,
   ImageUpload,
   CommentInput,
   BookmarkList,
   Button,
   Modal,
+  Icon,
 } from '../../components';
-import { useRef, useState } from 'react';
-import { allFolders } from '../../shared/DummyData';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Switch } from '../../pageComponents/folderupdateComponents/components';
+import { getFolder, updateFolder } from '../../apis/FolderAPI';
+import { SpecificFolder } from '../../types';
+import { listFiles } from '../../util/S3ImageHandler';
 
 const FolderUpdate = () => {
   const [isPrivate, setIsPrivate] = useState(false);
-  const [title, setTitle] = useState('');
-  const [tags, setTags] = useState([]);
+  const titleInput = useRef<HTMLInputElement>();
+  // const [tags, setTags] = useState([]);
   const [imageSrc, setImageSrc] = useState('');
-  const [content, setContent] = useState('');
+  const [isPinned, setIsPinned] = useState(false);
+  const contentInput = useRef<HTMLTextAreaElement>();
   const [bookmarks, setBookmarks] = useState([]);
   const [showBookmarkModal, setShowBookmarkModal] = useState(false);
+
+  const router = useRouter();
+  const { id } = router.query;
 
   const handleBookmark = () => {
     setShowBookmarkModal(!showBookmarkModal);
   };
 
-  const router = useRouter();
-  const { id } = router.query;
-
   const moveUserPage = () => {
     router.push(`/user/1`); // 추후 전역에서 사용하는 user id 붙이기
   };
 
-  const moveFolderDetailPage = () => {
+  const changePin = () => {
+    setIsPinned(!isPinned);
+  };
+
+  const moveFolderDetailPage = async () => {
+    const title = titleInput.current.value;
+    const content = contentInput.current.value;
+
+    await updateFolder({
+      id: Number(id),
+      title,
+      image: '',
+      content,
+      isPinned,
+      isPrivate,
+      tags: [],
+      bookmarks,
+    });
+    const res = await listFiles();
+    console.log(res);
     router.push(`/folderdetail/${id}`);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { isPrivate, title, image, content, bookmarks }: SpecificFolder =
+        await getFolder(Number(id));
+      setIsPrivate(isPrivate);
+      titleInput.current.value = title;
+      // setTags(folder.tags)
+      // setImageSrc(image);
+      contentInput.current.value = content;
+      setBookmarks(bookmarks);
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -44,18 +80,21 @@ const FolderUpdate = () => {
         closeFunc={handleBookmark}
       />
       <S.ContentContainer>
-        <Switch />
+        <S.SwitchContainer>
+          <Switch isPrivate={isPrivate} setIsPrivate={setIsPrivate} />
+          <S.IconWrapper onClick={changePin}>
+            <Icon name={isPinned ? 'pin_blue_ic' : 'ico_pin'} size={30} />
+          </S.IconWrapper>
+        </S.SwitchContainer>
         <S.TitleInput
           type="text"
           maxLength={20}
-          placeholder="제목을 입력하세요"
+          placeholder="제목을 입력해 주세요"
+          ref={titleInput}
         />
-        <ImageUpload version="page" />
-        <CommentInput version="update" />
-        <BookmarkList
-          version="update"
-          bookmarkItems={allFolders[0].bookmarks!}
-        />
+        <ImageUpload version="page" setImageSrc={setImageSrc} />
+        <CommentInput ref={contentInput} version="update" />
+        <BookmarkList version="update" bookmarkItems={bookmarks} />
         <S.ButtonWrapper>
           <S.BookmarkButton onClick={handleBookmark} type="button">
             북마크 추가하기 +
