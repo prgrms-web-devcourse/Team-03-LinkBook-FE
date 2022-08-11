8a45fb786a7309f13b3a2f97923c00c3cafb74fc
YongSangYou <dydtkd113@gmail.com>
2022-08-12 00:16:38 +0900

    fix: 타입 추가

--- web/types/index.ts
+++ web/types/index.ts
@@ -3,8 +3,10 @@ export type {
   AllFolderList,
   SpecificUserFolderList,
   SpecificFolder,
+  Folder,
   FolderCreateOrUpdate,
   GetFolderList,
   GetUserFolderList,
 } from './folder';
 export type { Comments, CreateOrUpdateComment } from './comment';
+export type { TabType } from './tab';
