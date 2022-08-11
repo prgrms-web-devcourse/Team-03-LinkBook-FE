d737952d6b736f2596c39a7a4611fffc8336baed
960817 <92906766+960817@users.noreply.github.com>
2022-08-11 23:14:49 +0900

    Feat: 로그인 api 연동

--- extension/src/pages/Root.tsx
+++ extension/src/pages/Root.tsx
@@ -1,14 +1,18 @@
 import React, { useLayoutEffect, useState } from "react";
-import { ACCESS_TOKEN } from "../utils/constants";
+import { ACCESS_TOKEN, REFRESH_TOKEN, URL } from "../utils/constants";
 import { getCookie } from "../utils/cookies";
 import LoginPage from "./LoginPage";
 import MainPage from "./MainPage";
 
 const Root = () => {
   const [isLogin, setIsLogin] = useState(false);
+
   useLayoutEffect(() => {
-    getCookie(ACCESS_TOKEN, "https://google.com").then((res) => {
+    getCookie(ACCESS_TOKEN, URL).then((res) => {
       if (res) return setIsLogin(true);
+      getCookie(REFRESH_TOKEN, URL).then((res) => {
+        if (res) return setIsLogin(true);
+      });
     });
   }, []);
 
