f36eb9542624604e79c4108738209d34fad2af1e
960817 <92906766+960817@users.noreply.github.com>
2022-08-11 16:03:52 +0900

    Style: 페이지 이름 변경, 쿠키 함수명 변경

--- extension/src/pages/Root.tsx
+++ extension/src/pages/Root.tsx
@@ -1,13 +1,14 @@
 import React, { useLayoutEffect, useState } from "react";
-import { getCookies } from "../utils/cookies";
+import { ACCESS_TOKEN } from "../utils/constants";
+import { getCookie } from "../utils/cookies";
 import LoginPage from "./LoginPage";
 import MainPage from "./MainPage";
 
 const Root = () => {
   const [isLogin, setIsLogin] = useState(false);
   useLayoutEffect(() => {
-    getCookies("ACCESS_TOKEN", "https://google.com").then((res) => {
-      if (res) setIsLogin(true);
+    getCookie(ACCESS_TOKEN, "https://google.com").then((res) => {
+      if (res) return setIsLogin(true);
     });
   }, []);
 
