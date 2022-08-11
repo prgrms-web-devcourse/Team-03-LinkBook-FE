ca3000574e7c494c52d1904ee4a74d52200989ed
Steadystudy <sgmin206@naver.com>
2022-08-11 15:32:25 +0900

    refactor: folder api 수정

--- web/apis/FolderAPI.ts
+++ web/apis/FolderAPI.ts
@@ -1,7 +1,13 @@
 import axios from '.';
-import { GetFolderList, GetUserFolderList } from '../types';
+import {
+  CreateOrUpdateFolder,
+  GetFolderList,
+  GetUserFolderList,
+} from '../types';
 import { FOLDERS, USER } from './url';
 
+const ACCESS_TOKEN =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJST0xFX1VTRVIiXSwiaXNzIjoicHJncm1zIiwiZXhwIjoxNjYwMjAxNzE0LCJpYXQiOjE2NjAxOTgxMTQsImVtYWlsIjoic2dtaW4yMDZAbmF2ZXIuY29tIn0.HqLi70wGMQYTSUHboX1swvUiA99WOSSbjEXp4F3h93QUIPtqPmdmqY2Phd2hvHA-LFZse1p1KIPeRm57XVCSCQ';
 // 폴더 리스트 전체 조회 (페이지, 정렬)
 export const getFolderList = async ({ page, size, sort }: GetFolderList) => {
   const res = await axios.get(`${FOLDERS}`, {
@@ -39,7 +45,11 @@ export const getUserFolderList = async ({
 
 // 특정 폴더 조회
 export const getFolder = async (id: number) => {
-  const res = await axios.get(`${FOLDERS}/${id}`);
+  const res = await axios.get(`${FOLDERS}/${id}`, {
+    headers: {
+      'Access-Token': ACCESS_TOKEN,
+    },
+  });
 
   console.log(res);
   return res;
@@ -47,8 +57,32 @@ export const getFolder = async (id: number) => {
 
 // 폴더 생성 (북마크까지) => 미개발
 // Headers : Access Token 필요
-export const createFolder = async () => {
-  const res = await axios.post(`${FOLDERS}`);
+export const createFolder = async ({
+  title,
+  image,
+  content,
+  isPinned,
+  isPrivate,
+  tags,
+  bookmarks,
+}: CreateOrUpdateFolder) => {
+  const res = await axios.post(
+    `${FOLDERS}`,
+    {
+      title,
+      image,
+      content,
+      isPinned,
+      isPrivate,
+      tags,
+      bookmarks,
+    },
+    {
+      headers: {
+        'Access-Token': ACCESS_TOKEN,
+      },
+    },
+  );
 
   console.log(res);
   return res;
@@ -56,8 +90,34 @@ export const createFolder = async () => {
 
 // 폴더 수정 (북마크까지) => 미개발
 // Headers : Access Token 필요
-export const updateFolder = async (id: number) => {
-  const res = await axios.put(`${FOLDERS}/${id}`);
+export const updateFolder = async ({
+  id,
+  title,
+  image,
+  content,
+  isPinned,
+  isPrivate,
+  tags,
+  bookmarks,
+}: CreateOrUpdateFolder) => {
+  console.log(id);
+  const res = await axios.put(
+    `${FOLDERS}/${id}`,
+    {
+      title,
+      image,
+      content,
+      isPinned,
+      isPrivate,
+      tags,
+      bookmarks,
+    },
+    {
+      headers: {
+        'Access-Token': ACCESS_TOKEN,
+      },
+    },
+  );
 
   console.log(res);
   return res;
