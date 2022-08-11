1c89ec9014d6e31cacfdba328a5cbdcf509c5792
Jungjjeong <sky990106@dgu.ac.kr>
2022-08-12 04:19:19 +0900

    Fix: LogoutPage 함수 상수 사용으로 변경

--- web/components/NavigationBar/LogoutSection/LogoutSection.tsx
+++ web/components/NavigationBar/LogoutSection/LogoutSection.tsx
@@ -2,32 +2,26 @@ import * as S from '../NavigationBar.style';
 import { Button } from '../../index';
 import { useSetRecoilState } from 'recoil';
 import { showModalStatus } from '../../../recoil/showModal';
+import {
+  showLoginModal,
+  showSignUpModal,
+} from '../../../constants/modal.constants';
 
 const LogoutSection = () => {
   const setShowModalStatus = useSetRecoilState(showModalStatus);
-
-  const handleLogin = () => {
-    setShowModalStatus({
-      Login: true,
-      SignUp: false,
-      FirstLogin: false,
-    });
-  };
-
-  const handleSignUp = () => {
-    setShowModalStatus({
-      Login: false,
-      SignUp: true,
-      FirstLogin: false,
-    });
-  };
 
   return (
     <>
       <S.Line>|</S.Line>
       <S.UserContainer>
-        <S.UserButton onClick={handleSignUp}>회원가입</S.UserButton>
-        <Button type="button" version="navBar" onClick={handleLogin}>
+        <S.UserButton onClick={() => setShowModalStatus(showSignUpModal)}>
+          회원가입
+        </S.UserButton>
+        <Button
+          type="button"
+          version="navBar"
+          onClick={() => setShowModalStatus(showLoginModal)}
+        >
           로그인
         </Button>
       </S.UserContainer>
