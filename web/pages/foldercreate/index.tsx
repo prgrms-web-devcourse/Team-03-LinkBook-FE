1f5bfa65b5b0073c12027a9c3316e3aef9f009ec
Steadystudy <sgmin206@naver.com>
2022-08-12 18:11:37 +0900

    feat: tag input 추가 및 token 추가

--- web/pages/foldercreate/index.tsx
+++ web/pages/foldercreate/index.tsx
@@ -5,6 +5,7 @@ import {
   BookmarkList,
   Button,
   Icon,
+  TagSelector,
 } from '../../components';
 import { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
@@ -13,19 +14,20 @@ import {
   BookmarkInput,
 } from '../../pageComponents/folderupdateComponents/components';
 import { createFolder } from '../../apis/FolderAPI';
-import { TEMP_TOKEN } from '../../constants/alert.constants';
 import { FOLDER_DEFAULT_IMAGE } from '../../constants/image.constants';
+import { getCookie } from '../../util/cookies';
 
 const FolderCreate = () => {
   const [isPrivate, setIsPrivate] = useState(false);
   const titleInput = useRef<HTMLInputElement>();
-  // const [tags, setTags] = useState([]);
+  const [tags, setTags] = useState([]);
   const [imageSrc, setImageSrc] = useState('');
   const [isPinned, setIsPinned] = useState(false);
   const contentInput = useRef<HTMLTextAreaElement>();
   const [bookmarks, setBookmarks] = useState([]);
 
   const router = useRouter();
+  const token = getCookie('ACCESS_TOKEN');
 
   const moveUserPage = () => {
     router.push(`/user/1`); // 추후 전역에서 사용하는 user id 붙이기
@@ -35,7 +37,7 @@ const FolderCreate = () => {
     setIsPinned(!isPinned);
   };
 
-  const moveFolderDetailPage = async () => {
+  const createFolderAPI = async () => {
     const title = titleInput.current.value;
     const content = contentInput.current.value;
     const image = imageSrc || FOLDER_DEFAULT_IMAGE;
@@ -47,11 +49,18 @@ const FolderCreate = () => {
         content,
         isPinned,
         isPrivate,
-        tags: [],
+        tags,
         bookmarks,
       },
-      TEMP_TOKEN,
+      token,
     );
+
+    return id;
+  };
+
+  const moveFolderDetailPage = async () => {
+    const id = await createFolderAPI();
+
     router.push(`/folderdetail/${id}`);
   };
 
@@ -64,6 +73,7 @@ const FolderCreate = () => {
             <Icon name={isPinned ? 'pin_blue_ic' : 'ico_pin'} size={30} />
           </S.IconWrapper>
         </S.SwitchContainer>
+        <TagSelector selectedTag={tags} setSelectedTag={setTags} />
         <S.TitleInput
           type="text"
           maxLength={20}
