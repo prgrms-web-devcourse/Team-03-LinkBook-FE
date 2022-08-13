66c0648a9a7fd85af3b5d3f4c6dd8264b090ddb8
YongSangYou <dydtkd113@gmail.com>
2022-08-13 18:51:59 +0900

    design: 배경 변경

--- web/pageComponents/mainPageComponents/components/MyFoldersAreaLogOut/MyFoldersAreaLogOut.tsx
+++ web/pageComponents/mainPageComponents/components/MyFoldersAreaLogOut/MyFoldersAreaLogOut.tsx
@@ -11,41 +11,39 @@ const MyFoldersAreaLogOut = () => {
   const setShowModal = useSetRecoilState(showModalStatus);
 
   return (
-    <>
-      <S.AreaContainer>
-        <S.BackgroundImage
-          src="/backgrounds/myFoldersArea.svg"
-          layout="fixed"
-          width={600}
-          height={550}
-        />
-        <S.ContentContainer>
-          <S.TextWrapper>
-            <S.Header>
-              지금 바로 <br />
-              <S.MainColor>나만의 북마크 폴더</S.MainColor>를<br />
-              만들어 보세요!
-            </S.Header>
-            <S.Description>
-              북마크 폴더를 공유하고 싶을 땐 전체 공개
-              <br />
-              나만 보고 싶을 땐 나만 보기 설정이 가능해요 😎
-            </S.Description>
-          </S.TextWrapper>
-          <S.ButtonWrapper>
-            <Button type="submit" onClick={() => setShowModal(showLoginModal)}>
-              로그인
-            </Button>
-            <S.SignUpButton
-              type="submit"
-              onClick={() => setShowModal(showSignUpModal)}
-            >
-              회원가입
-            </S.SignUpButton>
-          </S.ButtonWrapper>
-        </S.ContentContainer>
-      </S.AreaContainer>
-    </>
+    <S.AreaContainer>
+      <S.BackgroundImage
+        src="/backgrounds/myFoldersAreaLogOut.svg"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+      <S.ContentContainer>
+        <S.TextWrapper>
+          <S.Header>
+            지금 바로 <br />
+            <S.MainColor>나만의 북마크 폴더</S.MainColor>를<br />
+            만들어 보세요!
+          </S.Header>
+          <S.Description>
+            북마크 폴더를 공유하고 싶을 땐 전체 공개
+            <br />
+            나만 보고 싶을 땐 나만 보기 설정이 가능해요 😎
+          </S.Description>
+        </S.TextWrapper>
+        <S.ButtonWrapper>
+          <Button type="submit" onClick={() => setShowModal(showLoginModal)}>
+            로그인
+          </Button>
+          <S.SignUpButton
+            type="submit"
+            onClick={() => setShowModal(showSignUpModal)}
+          >
+            회원가입
+          </S.SignUpButton>
+        </S.ButtonWrapper>
+      </S.ContentContainer>
+    </S.AreaContainer>
   );
 };
 
