2a42b77afa3c8cf4afe7a99d32ed328c9e8ebbb4
YongSangYou <dydtkd113@gmail.com>
2022-08-13 13:46:02 +0900

    feat: (임시) 메인페이지로 리다이렉트

--- web/pages/folderlist/explore/[subTag].tsx
+++ web/pages/folderlist/explore/[subTag].tsx
@@ -1,26 +1,49 @@
-import { useRouter } from 'next/router';
-import FolderListDummy from '../../../shared/folderListPageDummy';
-import {
-  FolderList,
-  TagCategory,
-} from '../../../pageComponents/folderlistComponents/components';
-import styled from '@emotion/styled';
+// import { useRouter } from 'next/router';
+// import FolderListDummy from '../../../shared/folderListPageDummy';
+// import {
+//   FolderList,
+//   TagCategory,
+// } from '../../../pageComponents/folderlistComponents/components';
+// import styled from '@emotion/styled';
+// import { PAGE_URL } from '../../../constants/url.constants';
 
-const explore = () => {
-  const router = useRouter();
-  const { subTag } = router.query;
-  const label = `${subTag} 북마크 폴더 리스트 (${FolderListDummy.length})`;
-  return (
-    <Container>
-      <FolderList data={FolderListDummy} label={label}>
-        <TagCategory />
-      </FolderList>
-    </Container>
-  );
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/',
+    },
+  };
 };
 
-const Container = styled.div`
-  padding-top: 100px;
-`;
+const explore = () => {
+  // const router = useRouter();
+  // const { pathname, query, asPath } = router;
+  // const { mainTag, subTag } = query;
+  // router.push(
+  //   {
+  //     pathname: `${PAGE_URL.LIST}`,
+  //     query: {
+  //       mainTag,
+  //       subTag,
+  //     },
+  //   },
+  //   `${PAGE_URL.LIST}/explore/${subTag}`,
+  // );
+  return 'hello';
+  // const router = useRouter();
+  // const { subTag } = router.query;
+  // const label = `${subTag} 북마크 폴더 리스트 (${FolderListDummy.length})`;
+  // return (
+  //   <Container>
+  //     <FolderList data={FolderListDummy} label={label}>
+  //       <TagCategory />
+  //     </FolderList>
+  //   </Container>
+  // );
+};
+
+// const Container = styled.div`
+//   padding-top: 100px;
+// `;
 
 export default explore;
