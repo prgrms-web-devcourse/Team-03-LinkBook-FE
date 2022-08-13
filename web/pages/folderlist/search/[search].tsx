753ef9b13b790cabd1ed3709feda354825be6354
YongSangYou <dydtkd113@gmail.com>
2022-08-13 13:46:02 +0900

    feat: (임시) 메인페이지로 리다이렉트

--- web/pages/folderlist/search/[search].tsx
+++ web/pages/folderlist/search/[search].tsx
@@ -1,21 +1,30 @@
-import { useRouter } from 'next/router';
-import { FolderList } from '../../../pageComponents/folderlistComponents/components';
-import FolderListDummy from '../../../shared/folderListPageDummy';
-import styled from '@emotion/styled';
+// import { useRouter } from 'next/router';
+// import { FolderList } from '../../../pageComponents/folderlistComponents/components';
+// import FolderListDummy from '../../../shared/folderListPageDummy';
+// import styled from '@emotion/styled';
 
-const search = () => {
-  const router = useRouter();
-  const { search } = router.query;
-  const label = `'${search}' 검색 결과(${FolderListDummy.length})`;
-  return (
-    <Container>
-      <FolderList data={FolderListDummy} label={label} />
-    </Container>
-  );
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/',
+    },
+  };
 };
 
-const Container = styled.div`
-  padding-top: 200px;
-`;
+const search = () => {
+  return 'hello';
+  // const router = useRouter();
+  // const { search } = router.query;
+  // const label = `'${search}' 검색 결과(${FolderListDummy.length})`;
+  // return (
+  //   <Container>
+  //     <FolderList data={FolderListDummy} label={label} />
+  //   </Container>
+  // );
+};
+
+// const Container = styled.div`
+//   padding-top: 200px;
+// `;
 
 export default search;
