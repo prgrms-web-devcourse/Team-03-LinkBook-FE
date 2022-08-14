7d105d85806b7489dcb7f9bbd9ce9107af2e771b
Jungjjeong <sky990106@dgu.ac.kr>
2022-08-15 02:39:31 +0900

    Design: 고래 background Image 추가

--- web/pageComponents/mainPageComponents/components/UseInfo/UseInfo.style.ts
+++ web/pageComponents/mainPageComponents/components/UseInfo/UseInfo.style.ts
@@ -1,26 +1,32 @@
 import styled from '@emotion/styled';
 import { Button, Text } from '../../../../components';
 
-export const UseInfoContainer = styled.div`
+export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  gap: 10px;
+  align-items: flex-end;
   justify-content: center;
-  align-items: center;
-
   width: 100%;
   height: auto;
-  padding: 156px 0;
   margin-top: 156px;
+  padding: 150px 0;
   background-color: ${({ theme }) => theme.colors.mainLight[1]};
+`;
+
+export const UseInfoContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
   gap: 28px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Header = styled(Text)`
@@ -46,7 +52,7 @@ export const MainColorDes = styled(Description)`
 `;
 
 export const LogoText = styled(Text)`
-  margin-right: 2px;
+  margin-right: 5px;
   font-weight: 700;
   font-size: ${({ theme }) => theme.fontSize.t[0]};
   color: ${({ theme }) => theme.colors.white[0]};
@@ -65,8 +71,27 @@ export const ButtonWrapper = styled.div`
 export const LinkButton = styled(Button)`
   width: 100%;
   height: 70px;
-  padding: 26.5px 32px;
-  font-weight: 700;
-  font-size: ${({ theme }) => theme.fontSize.h[0]};
+  padding: 25px 32px;
+  font-weight: 600;
+  font-size: ${({ theme }) => theme.fontSize.b[0]};
   color: ${({ theme }) => theme.colors.white[0]};
+`;
+
+export const Whale3Wrapper = styled.div`
+  width: 180px;
+  height: 300px;
+  margin-right: 20px;
+  background: url('/backgrounds/whale3.svg');
+  background-repeat: no-repeat;
+  background-position: top right;
+  background-size: 80px;
+`;
+
+export const Whale2Wrapper = styled.div`
+  width: 200px;
+  height: 300px;
+  background: url('/backgrounds/whale2.svg');
+  background-repeat: no-repeat;
+  background-position: bottom left;
+  background-size: contain;
 `;
