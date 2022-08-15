293806d4161d3a767f66df2d204bd90c6fc14088
YongSangYou <dydtkd113@gmail.com>
2022-08-15 18:32:19 +0900

    fix: 마우스 클릭으로 태그 선택 안되는 버그 해결

--- web/components/TagSelector/TagSelector.style.ts
+++ web/components/TagSelector/TagSelector.style.ts
@@ -1,6 +1,14 @@
 import styled from '@emotion/styled';
 
-
 export const Container = styled.div`
   width: 100%;
+`;
+
+export const Dim = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100vh;
 `;
