1923e853b39394b3fe1116b96240855cd1070a58
YongSangYou <dydtkd113@gmail.com>
2022-10-03 16:46:16 +0900

    refactor: 불필요한 컴포넌트 제거

--- web/pageComponents/folderlistComponents/components/index.ts
+++ web/pageComponents/folderlistComponents/components/index.ts
@@ -1,3 +1,1 @@
-export { default as FolderList } from './FolderList';
-export { default as SearchBar } from './SearchBar';
 export { default as TagCategory } from './TagCategory';
--- web/pageComponents/folderlistComponents/index.ts
+++
@@ -1,3 +0,0 @@
-import SearchBar from './components/SearchBar';
-
-export default SearchBar;
